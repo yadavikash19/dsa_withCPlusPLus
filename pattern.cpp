c2761a0f0881e4ffcff379f0152168c0e2f28f52
Vikash Yadav <vikashydv19@gmail.com>
2025-04-04 17:18:44 +0530

    Interesting Patterns

--- pattern.cpp
+++ pattern.cpp
@@ -2,13 +2,103 @@
 using namespace std;
  
 int main(){
- 
-    for(int i=0; i<5; i++){
-        for(int j=0; j<=i; j++){
-            cout<<"*"<<" ";
+
+    // int n=5;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         if(j<n-i-1) cout<<" "<<" ";
+    //         else cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    //         * 
+    //       * *
+    //     * * *
+    //   * * * *
+    // * * * * *
+
+    int n=5;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<2*n-1; j++){
+            if(j<n-i-1 || j>n+i-1){
+                cout<<" "<<" ";
+            }
+            else{
+                cout<<"*"<<" ";
+            }
+            
         }
         cout<<endl;
     }
+    cout<<endl;
+    cout<<endl;
+    for(int i=-1; i<n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+            if(j<n-i-1 || j>n+i-1){
+                cout<<"*"<<" ";
+            }
+            else{
+                cout<<" "<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    //         *
+    //       * * *       
+    //     * * * * *     
+    //   * * * * * * *   
+    // * * * * * * * * * 
+
+
+    // * * * * * * * * * 
+    // * * * *   * * * * 
+    // * * *       * * * 
+    // * *           * * 
+    // *               * 
+
+    cout<<endl;
+    cout<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<2*n-1; j++){
+            if(j<i || j>2*n-i-2){
+                cout<<" "<<" ";
+            }
+            else{
+                cout<<"*"<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<<endl;
+    for(int i=1; i<n+1; i++){
+        for(int j=0; j<2*n-1; j++){
+            if(j<i || j>2*n-i-2){
+                cout<<"*"<<" ";
+            }
+            else{
+                cout<<" "<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+    // * * * * * * * * *
+    //   * * * * * * *
+    //     * * * * *
+    //       * * *       
+    //         *
+
+
+    // *               *
+    // * *           * *
+    // * * *       * * *
+    // * * * *   * * * *
+    // * * * * * * * * *
+    
  
     return 0;
 }
